642d4b820008046eb9ecc38bcab561ade9d9e1c6
Mason Stewart <masondesu@gmail.com>
2013-12-23 19:52:28 -0500

    Removed JS, added metadata

--- Gruntfile.js
+++ Gruntfile.js
@@ -380,7 +380,7 @@ module.exports = function (grunt) {
         'autoprefixer',
         'concat',
         'cssmin',
-        'uglify',
+        // 'uglify',
         'copy:dist',
         'rev',
         'usemin',
